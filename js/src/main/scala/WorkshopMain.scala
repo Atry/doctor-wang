82100c73a7667d106a2b1fd638c0ce566e8ab565
Yang Bo <atryyang@thoughtworks.com>
2015-12-28 23:19:04 +0800

    Adjust spacing

--- js/src/main/scala/WorkshopMain.scala
+++ js/src/main/scala/WorkshopMain.scala
@@ -16,35 +16,34 @@ import org.scalajs.dom.html.Table
 
 @JSExport
 object WorkshopMain {
-  
-  
+
   @dom
   def table(initialTitle: String): Binding[BindingSeq[Node]] = {
     val title = Var(initialTitle)
-    
-    <h3>{title.each}</h3>
+
+    <h3>{ title.each }</h3>
     <button onclick={ event: Event => title := title.get + "<a>" }>Modify title</button>
     <table border="1" cellPadding="5">
       <tbody>
         <tr>
           <td>Operation</td>
-          <td>{title.each}</td>
+          <td>{ title.each }</td>
         </tr>
       </tbody>
     </table>
   }
-  
+
   @dom
   def page = {
     val data: Var[String] = Var("initial data")
     <section>
-      {data.each}
+      { data.each }
       <button onclick={ event: Event => data := "Modified" }>Modify data</button>
       <h3>Header 3</h3>
       { table(data.each).each }
     </section>
   }
-  
+
   @JSExport
   def main(): Unit = {
     import org.scalajs.dom.document
